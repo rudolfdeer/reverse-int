6062bcdaed93080ba86781e9ed36be3a51facc6b
rudolfdeer <tomalichko@gmail.com>
2021-01-29 16:05:53 +0300

    feat: add function reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let number = String(Math.abs(n));
+    let res = "";
+
+    for (let i = number.length; i--; ) {
+        res += number[i];
+    }
+
+    return +res;
+};
